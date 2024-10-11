495e8c644329839ca3f7959bff7bf7460d9b7990
Weseunghyun <bible091192@gmail.com>
2024-10-11 19:47:40 +0900

    반복문 파트 완료

--- src/Loop/AplusBfive.java
+++ src/Loop/AplusBfive.java
@@ -1,29 +1,29 @@
 package Loop;
 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-//2439 별 찍기 - 2
-public class StarBlank {
+//10952 A+B -5
+public class AplusBfive {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt((br.readLine()));
 
-        for (int i = 0; i < n; i++) {
-            for (int j = (n-1); j>i ; j--) {
-                bw.write(" ");
-            }
-            for (int k = 0; k<=i; k++){
-                bw.write("*");
-            }
-            bw.flush();
-            bw.newLine();
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a==0 && b==0) break;
+
+            bw.write((a+b)+"\n");
         }
+        bw.flush();
     }
+
 }
--- src/Loop/Star.java
+++ src/Loop/Star.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+//2438 별찍기 -1
 public class Star {
 
     public static void main(String[] args) throws IOException {
