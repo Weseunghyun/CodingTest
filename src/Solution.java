1fff5de979579dfcf72d9cfbfafd9d2d72ade3a9
Weseunghyun <bible091192@gmail.com>
2024-10-31 09:57:02 +0900

    Level1 -  - 명예의전당 문제 풀이

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,6 @@
-import java.util.*;
-
-class Solution {
-    public int[] solution(int[] numbers) {
-        Set<Integer> numSet = new HashSet<>();
-
-        for(int i=0; i<numbers.length; i++){
-            for(int j=0; j<numbers.length; j++){
-                if (i!=j) {
-                    numSet.add(numbers[i] + numbers[j]);
-                }
-            }
-        }
-
-
-        int[] answer = numSet.stream().mapToInt(i->i).toArray();
+public class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = {};
         return answer;
     }
 }
---
+++ src/programmers/Lv1/138477. 명예의 전당/Honor.java
@@ -0,0 +1,41 @@
+package programmers;
+
+import java.util.Arrays;
+
+class Honor {
+    class Solution {
+        public int[] solution(int k, int[] score) {
+            int[] arr = new int[k];
+            int[] answer = new int[score.length];
+            int min = 2001;
+
+            for(int i=0; i<score.length; i++){
+                if (i<k){
+                    arr[i] = score[i];
+                    if(min > arr[i]){
+                        min = arr[i];
+                    }
+                    answer[i] = min;
+                }else {
+                    if (score[i] > min) {
+                        for (int j = 0; j < k; j++) {
+                            if (arr[j] == min) {
+                                arr[j] = score[i];
+                                break;
+                            }
+                        }
+                        min = Arrays.stream(arr).min().getAsInt();
+                    }
+                    answer[i] = min;
+                }
+            }
+            return answer;
+        }
+
+        public static void main(String[] args) {
+            Solution solution = new Solution();
+            System.out.println(
+                Arrays.toString(solution.solution(3, new int[]{10, 100, 20, 150, 1, 100, 200})));
+        }
+    }
+}
