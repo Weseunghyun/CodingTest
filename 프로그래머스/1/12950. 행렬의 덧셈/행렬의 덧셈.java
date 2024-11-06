7fed09c3ee3c7b2a0214efff6da8cec6ebb5fbad
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-11-07 08:57:35 +0900

    [level 1] Title: 행렬의 덧셈, Time: 3.49 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,11 +1,25 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr1[0].length];
-        for(int i=0;i<arr1.length; i++){
-            for(int j=0; j<arr1[i].length; j++){
-                answer[i][j] = arr1[i][j] + arr2[i][j];
+        //arr1의 행, 열로 반환할 2차원 배열 생성
+//         int[][] answer = new int[arr1.length][arr1[0].length];
+        
+//          for(int i=0; i<arr1.length; i++){
+//             for(int j=0; j<arr1[0].length; j++){
+//                 answer[i][j] = arr1[i][j] + arr2[i][j];
+//             }
+//         }
+        
+        // answer을 생성할 때 arr1으로 만들고 arr2의값만 더해줘도된다
+        //arr1의 값도 변경되어 훼손되는 문제가 있지만 코테니까 허용되는 수준이지 않을까
+        int[][] answer = {};
+        answer = arr1;
+        
+        for(int i=0; i<arr1.length; i++){
+            for(int j=0; j<arr1[0].length; j++){
+                answer[i][j] += arr2[i][j];
             }
         }
+       
         return answer;
     }
 }
