e487844791909d030599cc4a495df81a138bc7dc
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-11-06 21:00:40 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.15 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,10 +1,10 @@
 class Solution {
         public boolean solution(String s) {
-            int len = s.length();
+//             int len = s.length();
             
-            if(len!=4 && len!=6){
-                return false;
-            }
+//             if(len!=4 && len!=6){
+//                 return false;
+//             }
             
 //             char[] arr = s.toCharArray();
             
@@ -15,9 +15,18 @@ class Solution {
 //             }
 //             return true;
         
-            if(!s.matches("^[0-9]*$")){
-                return false;
-            }
-            return true;
+            //길이 검사까지하는것
+            //중괄호 내의 숫자는 앞에오는 문자가 4글자 | 또는 6글자가 오도록
+            //return s.matches("\\d{4}|\\d{6}");
+            //대괄호로 숫자 범위 표현
+            //return s.matches("[0-9]{4}|[0-9]{6}")
+
+
+            // if(!s.matches("^[0-9]*$")){
+            //     return false;
+            // }
+            // return true;
+            
+            return s.matches("\\d{4}|\\d{6}");
         }
     }
