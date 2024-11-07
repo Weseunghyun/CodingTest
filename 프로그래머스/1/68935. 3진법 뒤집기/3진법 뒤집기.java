c41bb87003b2a5e39dd754c51b82b4aca385a582
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-11-07 09:36:29 +0900

    Update 3진법 뒤집기.java
    주석 추가. 다른 방식으로 문제 푸는 방법

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -8,12 +8,21 @@ class Solution {
             sb.append(n%3);
             n/=3;
         }
-        
+
+        //문자 배열로 변환해서 풀어도 무방. 이게 더 속도 빠름
         char[] arr = sb.reverse().toString().toCharArray();
         int result = 0;
         for(int i = 0; i<arr.length; i++){
             result += (int)((arr[i] - '0') * Math.pow(3,i));
         }
+
+        // //StringBuilder의 charAt을 이용하여 풀어도 가능.
+        // sb.reverse();
+        // int result = 0;
+        // int length = sb.length();
+        // for (int i = 0; i < length; i++) {
+        //     result += (int) ((sb.charAt(i) - '0') * Math.pow(3, i));
+        // }
         
         return result;
     }
