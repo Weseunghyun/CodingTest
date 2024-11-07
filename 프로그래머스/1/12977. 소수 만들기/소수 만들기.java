ad9edd6638dd754bf45c1dfbbc7dc42cd5100e1c
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-11-07 10:13:07 +0900

    [level 1] Title: 소수 만들기, Time: 123.33 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int len = nums.length;
+        
+        for(int i=0; i<len-2; i++){
+            for(int j=i+1; j<len-1; j++){
+                for(int k=j+1; k<len; k++){
+                    int result = nums[i] + nums[j] + nums[k];
+                    int count = 0;
+                    for(int x=1; x<result; x++){
+                        if(result%x==0){
+                            count++;
+                        }
+                    }
+                    if(count==1){
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
