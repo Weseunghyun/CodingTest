e67bae36330f9a5276c6d6cd8ea50ec67e6749a0
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-11-08 11:57:32 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.96 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,24 +1,28 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        List<String> arr = new ArrayList<>();
-        arr.add("zero");
-        arr.add("one");
-        arr.add("two");
-        arr.add("three");
-        arr.add("four");
-        arr.add("five");
-        arr.add("six");
-        arr.add("seven");
-        arr.add("eight");
-        arr.add("nine");
-
-        for(int i=0; i<arr.size(); i++){
-            if(s.contains(arr.get(i))){
-                s = s.replaceAll(arr.get(i), String.valueOf(i));
-            }
+//         List<String> list = new ArrayList<>();
+//         list.add("zero");
+//         list.add("one"); 
+//         list.add("two"); 
+//         list.add("three");
+//         list.add("four"); 
+//         list.add("five"); 
+//         list.add("six");
+//         list.add("seven"); 
+//         list.add("eight"); 
+//         list.add("nine");
+        
+//         for(int i=0; i<list.size(); i++){
+//             if(s.contains(list.get(i))){
+//                 s = s.replaceAll(list.get(i), String.valueOf(i));
+//             }
+//         }
+//         return Integer.parseInt(s);
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-
         return Integer.parseInt(s);
     }
 }
