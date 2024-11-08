4d8a68ebf1004d39b6450a1a881fe05df237043e
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-11-08 10:53:26 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 2.44 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,22 +1,32 @@
 class Solution {
     public int solution(String t, String p) {
-        int answer = 0;
+//         int answer = 0;
+//         int length = p.length();
+        
+//         char[] arr = t.toCharArray();
+        
+//         String sample = "";
+//         for(int i=0; i<t.length()-length+1; i++){
+//             sample += arr[i];
+//             for(int j=1; j<length; j++){
+//                 sample += arr[i+j];
+//             }
+//             if(Long.parseLong(sample) <= Long.parseLong(p)){
+//                     answer++;
+//             }
+//             sample = "";
+//         }
+        
+//         return answer;
         int length = p.length();
-        
-        char[] arr = t.toCharArray();
-        
-        String sample = "";
-        for(int i=0; i<t.length()-length+1; i++){
-            sample += arr[i];
-            for(int j=1; j<length; j++){
-                sample += arr[i+j];
+        Long pValue = Long.parseLong(p);
+        int answer = 0;
+        for(int i=0; i<=t.length() - length; i++){
+            String sample = t.substring(i, length+i);
+            if(Long.parseLong(sample) <= pValue){
+                answer++;;
             }
-            if(Long.parseLong(sample) <= Long.parseLong(p)){
-                    answer++;
-            }
-            sample = "";
         }
-        
         return answer;
     }
 }
