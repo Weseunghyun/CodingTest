852ae491471ce12829d2a782a2386fa46fa3a433
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-11-08 11:55:14 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.83 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        List<String> arr = new ArrayList<>();
+        arr.add("zero");
+        arr.add("one");
+        arr.add("two");
+        arr.add("three");
+        arr.add("four");
+        arr.add("five");
+        arr.add("six");
+        arr.add("seven");
+        arr.add("eight");
+        arr.add("nine");
+
+        for(int i=0; i<arr.size(); i++){
+            if(s.contains(arr.get(i))){
+                s = s.replaceAll(arr.get(i), String.valueOf(i));
+            }
+        }
+
+        return Integer.parseInt(s);
+    }
+}
