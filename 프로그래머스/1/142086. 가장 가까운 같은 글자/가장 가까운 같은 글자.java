41a38c8db0300c8120af886b750fe9a7a5b89679
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-11-11 09:50:52 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.23 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,18 +1,27 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
-        int length = s.length();
-        char[] arr = s.toCharArray();
-        int[] answer = new int[length];
-        Arrays.fill(answer, -1);
-        for(int i=1; i<length; i++){
-            for(int j=0; j<i; j++){
-                if(arr[j] == arr[i]){
-                    answer[i] = i-j;
-                }
-            }
-        }
+//         int length = s.length();
+//         char[] arr = s.toCharArray();
+//         int[] answer = new int[length];
+//         Arrays.fill(answer, -1);
+//         for(int i=1; i<length; i++){
+//             for(int j=0; j<i; j++){
+//                 if(arr[j] == arr[i]){
+//                     answer[i] = i-j;
+//                 }
+//             }
+//         }
         
+//         return answer;
+        
+        int[] answer = new int[s.length()];
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            answer[i] = i-map.getOrDefault(ch,i+1);
+            map.put(ch,i);
+        }
         return answer;
     }
 }
