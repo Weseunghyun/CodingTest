b52802f472d281bf05524a3a5653e178702cf0ea
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-11-11 10:20:22 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        //a는 교환 병 수, b는 콜라 개수, n은 총 빈병 수
+        int answer = 0;
+        while(n/a>0){
+            answer += n/a * b;
+            
+            n = (n/a) * b + (n%a);
+        }
+        
+        return answer;
+    }
+}
