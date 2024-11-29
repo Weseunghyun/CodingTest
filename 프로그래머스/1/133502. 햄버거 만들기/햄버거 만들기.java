0732bd4cd9411ac6de4f944b81fccd8105c77f99
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-11-29 11:08:59 +0900

    [level 1] Title: 햄버거 만들기, Time: 17.02 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -2,28 +2,45 @@ import java.util.*;
 class Solution {
     //1빵 2야채 3고기 1231
     public int solution(int[] ing) {
-        List<Integer> list = new ArrayList<>();
+//         List<Integer> list = new ArrayList<>();
+//         int answer = 0;
+//         //1,2,3,1 을 찾는다
+//         //answer을 1 늘리고 배열에서 없앤다! 방금 포장한 햄버거를
+//         //다시 1,2,3,1 을 찾는다.
+        
+//         for(int i : ing){
+//             list.add(i);
+//             while(list.size() >= 4){
+//                 int n = list.size();
+//                 if(!(list.get(n-1) == 1
+//                   && list.get(n-2) == 3
+//                   && list.get(n-3) == 2
+//                   &&list.get(n-4) == 1)){
+//                     break;
+//                 }
+//                 for(int j=0; j<4; j++){
+//                         list.remove(list.size()-1);
+//                 }
+//                 answer++;
+//             }
+//         }
+//         return answer;
+        
         int answer = 0;
-        //1,2,3,1 을 찾는다
-        //answer을 1 늘리고 배열에서 없앤다! 방금 포장한 햄버거를
-        //다시 1,2,3,1 을 찾는다.
+        int[] stack = new int[ing.length];
+        int sp = 0;
         
         for(int i : ing){
-            list.add(i);
-            while(list.size() >= 4){
-                int n = list.size();
-                if(!(list.get(n-1) == 1
-                  && list.get(n-2) == 3
-                  && list.get(n-3) == 2
-                  &&list.get(n-4) == 1)){
-                    break;
-                }
-                for(int j=0; j<4; j++){
-                        list.remove(list.size()-1);
-                }
+            stack[sp++] = i;
+            if(sp>=4 && stack[sp-1] == 1
+              && stack[sp-2] == 3
+              && stack[sp-3] == 2
+              && stack[sp-4] == 1){
                 answer++;
+                sp-=4;
             }
         }
+        
         return answer;
     }
 }
