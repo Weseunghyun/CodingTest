cd9c9c0293277c70e50bc68912c09f86da9418f1
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-12-07 14:17:33 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.67 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,26 +1,42 @@
-import java.util.*;
-class Solution {
-    public String solution(String s) {
-        String[] sArr = s.split(" ", -1);
-        StringBuilder sb = new StringBuilder();
+// import java.util.*;
+// class Solution {
+//     public String solution(String s) {
+//         String[] sArr = s.split(" ", -1);
+//         StringBuilder sb = new StringBuilder();
        
-        for(int i=0; i<sArr.length; i++){
-            char[] cArr = sArr[i].toCharArray();
+//         for(int i=0; i<sArr.length; i++){
+//             char[] cArr = sArr[i].toCharArray();
             
-            for(int j=0; j<cArr.length; j++){
-                if(j==0){
-                    cArr[j] = Character.toUpperCase(cArr[j]);
-                } else {
-                    cArr[j] = Character.toLowerCase(cArr[j]);
-                }
-            }
+//             for(int j=0; j<cArr.length; j++){
+//                 if(j==0){
+//                     cArr[j] = Character.toUpperCase(cArr[j]);
+//                 } else {
+//                     cArr[j] = Character.toLowerCase(cArr[j]);
+//                 }
+//             }
             
-            sb.append(cArr);
-            if(i!=sArr.length-1){
-                sb.append(" ");
-            }
-        }
+//             sb.append(cArr);
+//             if(i!=sArr.length-1){
+//                 sb.append(" ");
+//             }
+//         }
         
-        return sb.toString();
-    }
+//         return sb.toString();
+//     }
+// }
+
+//효율 확인
+class Solution {
+  public String solution(String s) {
+        String answer = "";
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for(String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
+        }
+
+        return answer;
+  }
 }
