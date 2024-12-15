0eb29eac70e1226425666ec2afe5307ec3942041
WSK <127702076+Weseunghyun@users.noreply.github.com>
2024-12-16 06:47:38 +0900

    [level 2] Title: 귤 고르기, Time: 59.02 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,33 +1,33 @@
-import java.util.*;
+// import java.util.*;
 
-public class Solution {
-    public int solution(int k, int[] tangerine) {
+// public class Solution {
+//     public int solution(int k, int[] tangerine) {
        
-        Map<Integer,Integer> map = new HashMap<>();
+//         Map<Integer,Integer> map = new HashMap<>();
         
-        //map 에 종류별 개수를 담는다.
-        for(int tan : tangerine){
-            map.put(tan, map.getOrDefault(tan, 0) + 1);
-        }
+//         //map 에 종류별 개수를 담는다.
+//         for(int tan : tangerine){
+//             map.put(tan, map.getOrDefault(tan, 0) + 1);
+//         }
         
-        // 개수만 리스트로 추출 후 내림차순 정렬
-        List<Integer> counts = new ArrayList<>(map.values());
-        counts.sort(Collections.reverseOrder());
+//         // 개수만 리스트로 추출 후 내림차순 정렬
+//         List<Integer> counts = new ArrayList<>(map.values());
+//         counts.sort(Collections.reverseOrder());
         
-        int result = 0;
-        int numType = 0;
-        for(int count : counts){
-            result += count;
-            numType++;
+//         int result = 0;
+//         int numType = 0;
+//         for(int count : counts){
+//             result += count;
+//             numType++;
             
-            if(result>=k){
-                break;
-            }
-        }
+//             if(result>=k){
+//                 break;
+//             }
+//         }
         
-        return numType;
-    }
-}
+//         return numType;
+//     }
+// }
 
 /*
 k개의 귤을 상자에 담는다. 상자에 담을 때 크기의 종류가 최대한 적도록 담는다.
@@ -45,4 +45,40 @@ Map을 사용하면 크기값을 key 값 개수를 value로 둔다.
 따라서 많은 개수를 가진 귤을 먼저 담도록하려면
 map 의 value를 따로 List로 빼와서 내림차순 정렬을하고
 개수를 value만큼 더하게 했을 때 k보다 같거나 크다면 거기까지만 담으면된다.
+*/
+import java.util.*;
+public class Solution{
+     public int solution(int k, int[] tangerine) {
+         int[] sizeCount = new int[10000001];
+        
+         for(int tan : tangerine){
+             sizeCount[tan]++;
+         }
+         
+         List<Integer> countList = new ArrayList<>();
+         for(int count : sizeCount){
+             if(count != 0){
+                 countList.add(count);
+             }
+         }
+         
+         countList.sort(Collections.reverseOrder());
+         
+         int result = 0;
+         int answer = 0;
+         
+         for(int c : countList){
+             result += c;
+             answer++;
+             if(result >= k){
+                 break;
+             }
+         }
+         return answer;
+     }
+}
+/*
+tangerine 의 원소는 1부터~10,000,000 이다.
+tangerine 의 크기만큼의 배열을 생성하고 tangerine 에 들어있는 크기 값의 등장 횟수만큼
+해당 크기값을 인덱스로 두고 값을 증가시키고 풀어보자. Map 과는 다른 방식
 */
