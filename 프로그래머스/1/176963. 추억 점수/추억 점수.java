898dbf6794273f25e5d50b235a23a602ab1a9806
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-02-27 21:28:28 +0900

    [level 1] Title: 추억 점수, Time: 19.45 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        for(int i=0; i<photo.length; i++){
+            int sum = 0;
+            for(String ph : photo[i]) {
+                for(int j=0; j<name.length; j++){
+                    if(name[j].equals(ph)) {
+                        sum+=yearning[j];
+                        continue;
+                    }
+                }
+            }
+            answer[i] = sum;
+        }
+        return answer;
+    }
+}
+
+/*
+name 배열은 사람들의 이름이 담겨있음
+yearning 배열은 name 과 대응됨,
+photo 배열은 name 에 있는 사람들이 담겨있음
+그 때 사진에 담겨있는 사람들의 이름과 그리움 점수를 대입해서 총 점수를 알아내면됨.
+
+예시를 보니 사진 문자열 배열에 name 배열에 존재하지 않는 이름이 있을 수 있음
+그러니 name 에 존재하는 이름인지 확인하고 있다면 그 순서의 yearning 을 가져와서 더하면 됨.
+총 반복횟수는 photo 배열의 길이만큼 진행하면됨.
+
+*/
