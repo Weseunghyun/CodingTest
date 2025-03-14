31a29dd4a4d28c4a4321d6c5a7bd9cf1dc3a06e7
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-03-14 09:42:19 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,36 +1,27 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-       
-        //         int[] answer = new int[commands.length];
-        
-//         for(int i=0; i<commands.length; i++){
-//             int start = commands[i][0];
-//             int end = commands[i][1];
-//             int sortIdx = commands[i][2] - 1;
-            
-//             int[] example = new int[end-start+1];
-//             int idx = 0;
-//             for(int j=start-1; j<end; j++){
-//                 example[idx++] = array[j];  
-//             }
-            
-//             Arrays.sort(example);
-//             answer[i] = example[sortIdx];
-//         }
-        
-//         return answer;
-        
-        int[] answer = new int[commands.length];
+        int len = commands.length;
+        int[] answer = new int[len];
 
-        //이렇게 copyOfRange를 사용하면 array를 2,3번째 매개변수 사이로 나눠져서 복사하여 새로운 배열을 생성할 수 있음.
-        //하지만 속도는 더 느려진다는 점.
-        for(int i=0; i<commands.length; i++){
-            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(temp);
-            answer[i] = temp[commands[i][2]-1];
+        for(int i=0; i<len; i++) {
+            int firstIdx = commands[i][0] - 1;
+            int endIdx = commands[i][1];
+            int findIdx = commands[i][2] - 1;
+            int[] arr = new int[endIdx - firstIdx];
+            
+            int idx = 0;
+            for(int j=firstIdx; j<endIdx; j++) {
+                arr[idx] = array[j];
+                idx++;
+            }
+            
+            Arrays.sort(arr);
+            
+            answer[i] = arr[findIdx];
         }
-
+        
         return answer;
     }
 }
+
