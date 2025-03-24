2da67f6bab7a843571f421de00837e9cce2e6cf3
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-03-24 14:26:22 +0900

    [level 2] Title: 가장 큰 수, Time: 237.96 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,8 +4,8 @@ class Solution {
     public String solution(int[] numbers) {
         // 1. 숫자를 문자열 배열로 변환
         String[] strNumbers = Arrays.stream(numbers)
-                                    .mapToObj(String::valueOf)
-                                    .toArray(String[]::new);
+            .mapToObj(String::valueOf)
+            .toArray(String[]::new);
 
         // 2. 정렬 기준: (b + a).compareTo(a + b) (내림차순 정렬)
         Arrays.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
@@ -43,4 +43,21 @@ class Solution {
 사용하여 내림차순하고 성능을 위해 StringBuilder를 사용하여 하나의 문자열로 만든다.
 이 때 String joinedString = String.join("", strNumbers); 메서드를 사용해서 만들 수도 있다.
 그 후 리턴해주면 끝
+
+Arrays.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
+[3, 30, 34, 5, 9]
+sort 메서드가 어떻게 정렬을 해주는걸까?
+우리는 매개변수에 집중해야한다. 우리는 compareTo를 이용할 뿐이지 b+a 와 a+b 의 위치를 바꾸려는 것이 아니다
+매개변수로 들어가는 a,b 의 위치를 서로 바꿀것인지 안바꿀 것인지 결정할 뿐이다!
+a,b 로 들어가는데 예를들어 a=3, b=30 이라고하자
+compareTo 메서드는 같을 경우 0, 앞에오는 매개변수가 클 경우 양수가 나와 위치를 바꾸고 작을경우 음수가 나오고 그대로 둔다
+그렇다면 (b+a).compareTo(a+b) 로 되어있으니
+"303".compareTo("330") 이 된다. 이 경우 앞에있는 숫자가 더 작으므로 음수가 나오고 sort 메서드가 순서를 바꾸지 않는다
+이 때 우리가 매개변수를 a,b 로 넣었기 때문에 해당 순서는 바뀌지 않는다 즉 3,30 으로 넣었을 때는 바뀌지 않는다는 것이다.
+
+만약 a=30, b=3 으로 들어갔다면? "330".compareTo("303") 이 되기때문에 앞에잇는 숫자가 커 양수가 나와 둘의 위치를 바꾼다
+즉 두 경우를 다 따져보아도 3,30 으로 들어가면 바꾸지않고 30,3 으로 들어가면 둘의 위치가 바뀌게 되기 때문에
+내림차순 정렬이 자동적으로 된다.
+
+다른 모두 경우도 동일하다. 3, 9 일때는 93.compareTo(39) 앞에있는게 크기 때문에 양수가 나와 9가 앞으로 가는 내림차순 정렬이 일어난다.
 */
