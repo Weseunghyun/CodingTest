d94a5a0c9e2b7623772beeb153ee7ca85764e152
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-09 13:54:34 +0900

    Title: 실수 받아 그대로 출력, Time: 145ms, Memory: 11MB, Status: Passed - Codetree

--- 250409/실수 받아 그대로 출력/enter-real-value-and-print.java
+++ 250409/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -5,7 +5,7 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
         double d = sc.nextDouble();
-        d = Math.round(d);
+        d = (Math.round(d*100)/100.0);
         System.out.printf("%.2f", d);
     }
 }
