9e378af0bcb4380f7e5aa3c789b618946d8fc4d9
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-10 16:47:12 +0900

    Title: 합을 복사하기, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250410/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a=1,b=2,c=3;
+        int d = a+b+c;
+
+        a=b=c=d;
+
+        System.out.printf("%d %d %d", a,b,c);
+    }
+}
