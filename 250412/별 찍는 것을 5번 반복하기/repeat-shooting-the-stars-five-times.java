894483d35dfc61384f1cdbc8d1f4ce35cbd54be4
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-12 06:00:12 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 82ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250412/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        print();
+    }
+
+    public static void print() {
+        for(int i=0; i<5; i++) {
+            System.out.println("**********");
+        }
+    }
+}
