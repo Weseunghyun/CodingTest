1d632ed5fa5e944903655783c0000d5a46bb7bb8
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-13 18:17:40 +0900

    Title: 문자열 정렬, Time: 108ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250413/문자열 정렬/string-sort.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        // Please write your code here.
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        
+        System.out.println(String.valueOf(arr));
+    }
+}
