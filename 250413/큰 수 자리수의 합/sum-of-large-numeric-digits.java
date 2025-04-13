dc7ceefb0916e489cc78fe941b788dc576d305a4
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-13 16:42:13 +0900

    Title: 큰 수 자리수의 합, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250413/큰 수 자리수의 합/sum-of-large-numeric-digits.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Please write your code here.
+        int n = a*b*c;
+        System.out.println(zarrisu(n));
+    }
+
+    public static int zarrisu(int n) {
+        if(n/10 <= 0) {
+            return n%10;
+        }
+
+        return n%10 + zarrisu(n/10);
+    }
+}
