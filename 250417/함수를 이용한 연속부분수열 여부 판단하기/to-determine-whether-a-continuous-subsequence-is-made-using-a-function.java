29ffb2031a50b26f66e99120279eaab7a7c03f65
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-17 20:53:48 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250417/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int[] a = new int[n1];
+        int[] b = new int[n2];
+        for (int i = 0; i < n1; i++)
+            a[i] = sc.nextInt();
+        for (int i = 0; i < n2; i++)
+            b[i] = sc.nextInt();
+        // Please write your code here.
+
+        boolean answer = isLinked(a,b);
+
+        if (answer) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean isLinked(int[] a, int[] b) {
+        boolean isFirst = false;
+        boolean isRight = true;
+        int bIdx = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == b[0]) {
+                isFirst = true;
+            }
+
+            if (bIdx < b.length) {
+                if (isFirst) {
+                    if (a[i] != b[bIdx]) {
+                        return !isRight;
+                    }
+                    bIdx++;
+                }
+            }
+        }
+
+        return isRight;
+    }
+}
