9565f652402b01f7d9e6afd2ce811eaefee9ecf4
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-24 11:40:15 +0900

    Title: 두수의 곱과 몫, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

--- 250424/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
+++ 250424/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt() + 8;
-        int b = sc.nextInt() * 3;
-        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
         System.out.println(a+" * " + b + " = " + (a*b));
         System.out.println(a+" / " + b + " = " + (a/b));
 
