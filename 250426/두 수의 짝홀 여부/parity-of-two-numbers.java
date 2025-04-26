d73389261d4a4648460aac824fd21fb11cbca872
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-26 23:29:43 +0900

    Title: 두 수의 짝홀 여부, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250426/두 수의 짝홀 여부/parity-of-two-numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a%2==1) {
+            System.out.println("odd");
+        } else {
+            System.out.println("even");
+        }
+
+        if (b%2==1) {
+            System.out.println("odd");
+        } else {
+            System.out.println("even");
+        }
+
+    
+    }
+}
