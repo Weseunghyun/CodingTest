ed46bee43441e781be669711a55d9d3dfa701a29
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-27 18:24:44 +0900

    Title: 증가시키며 출력하기, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 250427/증가시키며 출력하기/increase-and-print.java
+++ 250427/증가시키며 출력하기/increase-and-print.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+//        int n = sc.nextInt();
         for (int i = 5; i <= 17; i+=2) {
             System.out.print(i + " ");
         }
