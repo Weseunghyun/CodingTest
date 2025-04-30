e6c945c2adf581f3da1561c597835c52a09c091d
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 18:23:28 +0900

    Title: n부터 1까지 정수 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250430/n부터 1까지 정수 출력/print-n-to-1.java
+++ 250430/n부터 1까지 정수 출력/print-n-to-1.java
@@ -1,11 +1,1 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        while (n >= 1) {
-            System.out.println(n);
-            n--;
-        }
-    }
-}
+import
