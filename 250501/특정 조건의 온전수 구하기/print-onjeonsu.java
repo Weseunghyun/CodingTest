4d5bb78d63d0e2a80b46ce94b595f0aa55554236
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-05-01 01:01:08 +0900

    Title: 특정 조건의 온전수 구하기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250501/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (isPerfectNumber(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    
+    private static boolean isPerfectNumber(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
