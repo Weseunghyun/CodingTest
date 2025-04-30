6cc29e161354ff2af58fe4770cdcd4ed08bd8fcf
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 18:31:18 +0900

    Title: n부터 1까지 정수 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250430/n부터 1까지 정수 출력/print-n-to-1.java
+++ 250430/n부터 1까지 정수 출력/print-n-to-1.java
@@ -3,10 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a positive integer: ");
         int n = sc.nextInt();
-        while (n > 0) {
-            System.out.println(n--);
+        
+        while (n >= 1) {
+            System.out.println(n);
+            n--;
+        }
+    }
+}
+
+답변 :
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        while (n >= 1) {
+            System.out.println(n);
+            n--;
         }
     }
 }
