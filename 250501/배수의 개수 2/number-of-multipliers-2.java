7beb7e51d7203666a242a59958ba22885ec64eaa
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-05-01 00:29:56 +0900

    Title: 배수의 개수 2, Time: 117ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250501/배수의 개수 2/number-of-multipliers-2.java
+++ 250501/배수의 개수 2/number-of-multipliers-2.java
@@ -1,17 +1,17 @@
-import java.util.*;
-public class Main{
-    public static void main(String[] args){
-        Scanner in=new Scanner(System.in);
-        
-        //배수의 개수
-        int count_even=0;
-        
-        for(int i=1;i<=10;i++){
-            if(in.nextInt()%2==0){
-                count_even++;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int countEven = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            if (in.nextInt() % 2 == 0) {
+                countEven++;
             }
-            
         }
-        System.out.println(count_even);
+
+        System.out.println(countEven);
     }
 }
