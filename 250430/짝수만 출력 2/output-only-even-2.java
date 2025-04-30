1e49e3df334a4621863d31df459b1e0153ef2074
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 18:52:36 +0900

    Title: 짝수만 출력 2, Time: 94ms, Memory: 9MB, Status: Runtime Error - Codetree

--- 250430/짝수만 출력 2/output-only-even-2.java
+++ 250430/짝수만 출력 2/output-only-even-2.java
@@ -1,1 +1,23 @@
-import
+package com.company;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int B, A;
+        System.out.print("Enter the value of B: ");
+        B = sc.nextInt();
+        System.out.print("Enter the value of A: ");
+        A = sc.nextInt();
+        if (A >= B) {
+            while (B <= A) {
+                System.out.println(B);
+                B++;
+            }
+        } else {
+            while (A <= B) {
+                System.out.println(A);
+                A++;
+            }
+        }
+    }
+}
