73f1c399b57f1b87f85ef7153092ae468d749093
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 21:31:27 +0900

    Title: A/B 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250430/A/B 출력/a-divide-b.java
+++ 250430/A/B 출력/a-divide-b.java
@@ -1,21 +1,12 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Scanner;
 
-public class Main {
+public class A_B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        // 정수 a와 b를 BigDecimal로 변환
-        BigDecimal bigA = new BigDecimal(a);
-        BigDecimal bigB = new BigDecimal(b);
-
-        // 나눗셈 수행 (소수점 21자리까지, 내림 처리)
-        BigDecimal result = bigA.divide(bigB, 20, RoundingMode.DOWN);
-
-        // 결과 출력
-        System.out.println(result.toPlainString()); // 지수 표현 없이 출력
+        double result = (double)a / b;
+        System.out.printf("%.21f\n", result);
     }
 }
