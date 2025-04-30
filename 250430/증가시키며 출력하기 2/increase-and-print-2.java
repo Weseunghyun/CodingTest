bedb8d732ea29204a9c1fd0ec8fa10cfd4c4a6e2
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 17:23:16 +0900

    Title: 증가시키며 출력하기 2, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250430/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+
+        while (a < 27) {
+            System.out.print(a + " ");
+            a++;
+        }
+    }
+}
