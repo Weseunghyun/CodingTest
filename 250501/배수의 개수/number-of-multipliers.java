35c821b44927ff1088f1d86e520b1455c0421a42
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-05-01 00:43:52 +0900

    Title: 배수의 개수, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250501/배수의 개수/number-of-multipliers.java
+++ 250501/배수의 개수/number-of-multipliers.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+        int count3 = 0;
+        int count5 = 0;
         
-        int count3 = 0, count5 = 0;
         for (int i = 0; i < 10; i++) {
-            int num = sc.nextInt();
-            
-            if (num % 3 == 0) {
+            num1 = sc.nextInt();
+            if (num1 % 3 == 0) {
                 count3++;
             }
-            if (num % 5 == 0) {
+            if (num1 % 5 == 0) {
                 count5++;
             }
         }
