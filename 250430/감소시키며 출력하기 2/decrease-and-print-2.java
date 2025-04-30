404787e4477217d6f64d3066bf644b0320f13fd3
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 17:28:28 +0900

    Title: 감소시키며 출력하기 2, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250430/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+
+        for (int i = 26; i >=10; i--) {
+     
+                System.out.print(i+" ");
+            
+
+        }
+    }
+}
