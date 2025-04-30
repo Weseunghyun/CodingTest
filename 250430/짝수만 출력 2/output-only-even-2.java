7927fef2bba665647e178d2a4fa0b53e3a2ee6aa
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 20:25:43 +0900

    Title: 짝수만 출력 2, Time: 117ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250430/짝수만 출력 2/output-only-even-2.java
+++ 250430/짝수만 출력 2/output-only-even-2.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int AAA = sc.nextInt();
-        int BBB = sc.nextInt();
-
-        int i = AAA;
-        while (i <= BBB) {
-            if (i % 2 == 0) {
+        int A, B;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        int i = 2*A - 1; // 짝수 범위를 포함하는 숫자의 합을 구한다
+        while(i <= B){
+            if(i % 2 == 0) { // 짝수일 경우에만 출력
                 System.out.print(i + " ");
             }
-            i++;
+            i += 2; // 다음 숫자로 이동
         }
     }
 }
