501310b5c33f6a37a9c363f766e15920f5d34600
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 23:54:47 +0900

    Title: 자연수면 출력, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 250430/자연수면 출력/output-only-natural-number.java
+++ 250430/자연수면 출력/output-only-natural-number.java
@@ -1,16 +1,16 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        if (A > 0) {
-            for (int i = 1; i <= B; i++) {
-                System.out.print(A);
-            }
-        } else {
-            System.out.println("Invalid input");
-        }
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if (a > 0) {
+			for (int i = 0; i < b; i++) {
+				System.out.print(a);
+			}
+		} else {
+			System.out.println(0);
+		}
+	}
 }
