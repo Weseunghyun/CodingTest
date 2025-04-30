7e887e1f7124cb0efdafa49678f69d3b17e9ea79
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-05-01 00:44:51 +0900

    Title: 청소당번 정하기, Time: 118ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250501/청소당번 정하기/cleaning-numbering.java
+++ 250501/청소당번 정하기/cleaning-numbering.java
@@ -1,18 +1,19 @@
 import java.util.*;
+
 public class Main {
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-int n = sc.nextInt();
-long[] counts = new long[3];
-for (int i = 1; i <= n; i++) {
-if (i % 2 == 0) { // 교실을 청소합니다.
-counts[0]++;
-} else if (i % 3 == 0) { // 복도를 청소합니다.
-counts[1]++;
-} else { // 화장실을 청소합니다.
-counts[2]++;
-}
-}
-System.out.println(counts[0]+" "+counts[1]+" "+counts[2]);
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] counts = new long[3];
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) { // 교실을 청소합니다.
+                counts[0]++;
+            } else if (i % 3 == 0) { // 복도를 청소합니다.
+                counts[1]++;
+            } else { // 화장실을 청소합니다.
+                counts[2]++;
+            }
+        }
+        System.out.println(counts[0]+" "+counts[1]+" "+counts[2]);
+    }
 }
