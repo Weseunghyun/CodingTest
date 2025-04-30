63a837ddd35498936bb935f469bddcf9860a93bd
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 20:28:36 +0900

    Title: 짝수만 출력 2, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 250430/짝수만 출력 2/output-only-even-2.java
+++ 250430/짝수만 출력 2/output-only-even-2.java
@@ -6,12 +6,11 @@ public class Main {
         int A, B;
         A = sc.nextInt();
         B = sc.nextInt();
-        int i = 2*A - 1; // 짝수 범위를 포함하는 숫자의 합을 구한다
-        while(i <= B){
-            if(i % 2 == 0) { // 짝수일 경우에만 출력
-                System.out.print(i + " ");
-            }
-            i += 2; // 다음 숫자로 이동
+        int i = A;
+
+        while(i>=B){
+            System.out.print(i + " ");
+            i-=2;
         }
     }
 }
