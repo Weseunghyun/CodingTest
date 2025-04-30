5652279fc5a14862f63ced498171d24919afb8a4
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 19:06:24 +0900

    Title: 짝수만 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250430/짝수만 출력 2/output-only-even-2.java
+++ 250430/짝수만 출력 2/output-only-even-2.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
-public class Main {
+public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if (n < 1000) {
-            System.out.println("Easy");
-        } else if (n >= 1000 && n < 5000) {
-            System.out.println("Medium");
-        } else {
-            System.out.println("Hard");
+        int AAA = sc.nextInt();
+        int BBB = sc.nextInt();
+
+        int i = AAA;
+        while (i <= BBB) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+            i++;
         }
     }
 }
