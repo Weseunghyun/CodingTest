94acf6e978863fdd23710269d14569f589995748
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-04-30 23:08:58 +0900

    Title: N번 더하기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250430/N번 더하기/add-n-times.java
+++ 250430/N번 더하기/add-n-times.java
@@ -1,18 +1,12 @@
-import java.util.Scanner;
-
-public class N번더하기 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int A, NA, N;
-        
-        A = sc.nextInt();
-        NA = sc.nextInt();
-        N = sc.nextInt();
-        
-        for (int i = 0; i < N; i++) {
-            System.out.println(A + NA);
-            A += NA;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            a+=n;
+            System.out.println(a);
         }
     }
 }
