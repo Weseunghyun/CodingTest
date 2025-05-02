798be8e9917d61077e1718677de305ce9a5b37bd
WSK <127702076+Weseunghyun@users.noreply.github.com>
2025-05-02 16:29:38 +0900

    Title: A부터 B까지의 합, Time: 112ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250502/A부터 B까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int count=0;
+        for(int i=0;i<5;i++){
+            if(sc.nextInt()%2==0)count++;
+        }
+        System.out.println(count);
+    }
+}
